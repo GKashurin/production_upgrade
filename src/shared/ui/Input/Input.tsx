621030a8ef7518c17b426db71879046f0facc951
GKashurin <gkash@e1.ru>
2022-10-15 18:34:39 +0500

    add input, LoginForm

---
+++ src/shared/ui/Input/Input.tsx
@@ -0,0 +1,56 @@
+import {classNames} from 'shared/lib/classNames/classNames'
+import cls from './Input.module.scss'
+import {InputHTMLAttributes, memo, useEffect, useRef, useState} from "react";
+
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>
+
+interface InputProps extends HTMLInputProps {
+	className?: string;
+	value?: string;
+	onChange?: (value: string) => void;
+}
+
+export const Input = memo((props: InputProps) => {
+	const {
+		className,
+		value,
+		onChange,
+		type = 'text',
+		placeholder,
+		autoFocus,
+		...otherProps
+	} = props;
+
+	const ref = useRef<HTMLInputElement>(null)
+	const [isFocused, setIsFocused] = useState(false)
+
+	const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+		onChange?.(e.target.value)
+	}
+
+	useEffect(() => {
+		if (autoFocus) {
+			setIsFocused(true)
+			ref.current?.focus()
+		}
+	}, [autoFocus])
+
+	return (
+		<div className={classNames(cls.InputWrapper, {}, [className])}>
+			{placeholder && (
+				<div className={cls.placeholder}>
+					{placeholder + '>'}
+				</div>
+			)}
+			<input
+				ref={ref}
+				type={type}
+				value={value}
+				onChange={onChangeHandler}
+				className={cls.input}
+				autoFocus={isFocused}
+				{...otherProps}
+			/>
+		</div>
+	)
+})
