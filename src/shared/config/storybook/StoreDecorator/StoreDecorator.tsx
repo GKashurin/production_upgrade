a1da4e368829716a17100803601523cdfbeb7713
GKashurin <gkash@e1.ru>
2022-12-18 14:16:16 +0500

    fix imports

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,20 +1,12 @@
 import { Story } from '@storybook/react';
-// eslint-disable-next-line import-checker-eslint-plugin/public-api-imports
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
-// eslint-disable-next-line import-checker-eslint-plugin/public-api-imports
-import { loginReducer } from '@/features/AuthByUsername/model/slice/loginSlice';
-// eslint-disable-next-line import-checker-eslint-plugin/public-api-imports
+import { loginReducer } from '@/features/AuthByUsername/testing';
 import { ReducersList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-// eslint-disable-next-line import-checker-eslint-plugin/public-api-imports
-import { articleDetailsReducer } from '@/entities/Article/model/slice/articleDetailsSlice';
-// eslint-disable-next-line import-checker-eslint-plugin/public-api-imports
-import { addCommentFormReducer } from '@/features/AddCommentForm/model/slice/addCommentFormSlice';
-// eslint-disable-next-line import-checker-eslint-plugin/public-api-imports
-import { articlesPageReducer } from '@/pages/ArticlesPage/model/slice/articlesPageSlice';
-// eslint-disable-next-line import-checker-eslint-plugin/public-api-imports
-import { articleDetailsPageReducer } from '@/pages/ArticleDetailsPage/model/slice';
-// eslint-disable-next-line import-checker-eslint-plugin/public-api-imports
-import { profileReducer } from '@/features/EditableProfileCard/model/slice/profileSlice';
+import { articleDetailsReducer } from '@/entities/Article/testing';
+import { addCommentFormReducer } from '@/features/AddCommentForm/testing';
+import { articlesPageReducer } from '@/pages/ArticlesPage/testing';
+import { articleDetailsPageReducer } from '@/pages/ArticleDetailsPage/testing';
+import { profileReducer } from '@/features/EditableProfileCard/testing';
 
 const defaultAsyncReducers: ReducersList = {
     loginForm: loginReducer,
