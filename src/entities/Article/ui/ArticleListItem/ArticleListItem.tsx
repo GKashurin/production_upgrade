c177f0bd95e70b378c50784f661c33d63a40a76c
GKashurin <gkash@e1.ru>
2022-12-30 19:10:51 +0500

    fix story

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -33,74 +33,74 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
 
     const types = <Text text={article.type.join(', ')} className={cls.types} />;
     const views = (
-      <>
-          <Text text={String(article.views)} className={cls.views} />
-          <Icon Svg={EyeIcon} />
-      </>
+        <>
+            <Text text={String(article.views)} className={cls.views} />
+            <Icon Svg={EyeIcon} />
+        </>
     );
 
     if (view === ArticleView.BIG) {
         const textBlock = article.blocks.find(
-          (block) => block.type === ArticleBlockType.TEXT,
+            (block) => block.type === ArticleBlockType.TEXT,
         ) as ArticleTextBlock;
 
         return (
-          <div className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}>
-              <Card className={cls.card}>
-                  <div className={cls.header}>
-                      <Avatar size={30} src={article.user.avatar} />
-                      <Text text={article.user.username} className={cls.username} />
-                      <Text text={article.createdAt} className={cls.date} />
-                  </div>
-                  <Text title={article.title} className={cls.title} />
-                  {types}
-                  <AppImage
-                    fallback={<Skeleton width="100%" height={250} />}
-                    src={article.img}
-                    className={cls.img}
-                    alt={article.title}
-                  />
-                  {textBlock && (
-                    <ArticleTextBlockComponent block={textBlock} className={cls.textBlock} />
-                  )}
-                  <div className={cls.footer}>
-                      <AppLink
-                        target={target}
-                        to={getRouteArticleDetails(article.id)}
-                      >
-                          <Button theme={ButtonTheme.OUTLINE}>
-                              {t('Читать далее...')}
-                          </Button>
-                      </AppLink>
-                      {views}
-                  </div>
-              </Card>
-          </div>
+            <div className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}>
+                <Card className={cls.card}>
+                    <div className={cls.header}>
+                        <Avatar size={30} src={article.user.avatar} />
+                        <Text text={article.user.username} className={cls.username} />
+                        <Text text={article.createdAt} className={cls.date} />
+                    </div>
+                    <Text title={article.title} className={cls.title} />
+                    {types}
+                    <AppImage
+                        fallback={<Skeleton width="100%" height={250} />}
+                        src={article.img}
+                        className={cls.img}
+                        alt={article.title}
+                    />
+                    {textBlock && (
+                        <ArticleTextBlockComponent block={textBlock} className={cls.textBlock} />
+                    )}
+                    <div className={cls.footer}>
+                        <AppLink
+                            target={target}
+                            to={getRouteArticleDetails(article.id)}
+                        >
+                            <Button theme={ButtonTheme.OUTLINE}>
+                                {t('Читать далее...')}
+                            </Button>
+                        </AppLink>
+                        {views}
+                    </div>
+                </Card>
+            </div>
         );
     }
 
     return (
-      <AppLink
-        target={target}
-        to={getRouteArticleDetails(article.id)}
-        className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}
-      >
-          <Card className={cls.card}>
-              <div className={cls.imageWrapper}>
-                  <AppImage
-                    fallback={<Skeleton width={200} height={200} />}
-                    alt={article.title}
-                    src={article.img}
-                    className={cls.img}
-                  />
-                  <Text text={article.createdAt} className={cls.date} />
-              </div>
-              <div className={cls.infoWrapper}>
-                  {types}
-                  {views}
-              </div>
-              <Text text={article.title} className={cls.title} />
-          </Card>
-      </AppLink>
+        <AppLink
+            target={target}
+            to={getRouteArticleDetails(article.id)}
+            className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}
+        >
+            <Card className={cls.card}>
+                <div className={cls.imageWrapper}>
+                    <AppImage
+                        fallback={<Skeleton width={200} height={200} />}
+                        alt={article.title}
+                        src={article.img}
+                        className={cls.img}
+                    />
+                    <Text text={article.createdAt} className={cls.date} />
+                </div>
+                <div className={cls.infoWrapper}>
+                    {types}
+                    {views}
+                </div>
+                <Text text={article.title} className={cls.title} />
+            </Card>
+        </AppLink>
     );
 });
